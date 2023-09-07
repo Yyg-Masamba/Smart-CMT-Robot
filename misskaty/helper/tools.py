ed21928a292865cf81b5d362c9d644b6e651d8d0
Pea Masamba <129601224+Yyg-Masamba@users.noreply.github.com>
2023-09-07 23:49:39 +0700

    fix

--- misskaty/helper/tools.py
+++ misskaty/helper/tools.py
@@ -14,7 +14,7 @@ from misskaty.helper.http import fetch
 from misskaty.helper.human_read import get_readable_time
 from misskaty.plugins import ALL_MODULES
 
-LOGGER = logging.getLogger("MissKaty")
+LOGGER = logging.getLogger("Pea Masamba")
 URL_REGEX = r"(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])"
 GENRES_EMOJI = {
     "Action": "👊",
