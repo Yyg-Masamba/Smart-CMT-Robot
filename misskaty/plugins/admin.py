e5d77c6ed7e2472c5b2458a1125deb6ff1618ef3
Pea Masamba <129601224+Yyg-Masamba@users.noreply.github.com>
2023-09-07 13:58:20 +0700

    Fix

--- misskaty/plugins/admin.py
+++ misskaty/plugins/admin.py
@@ -55,7 +55,7 @@ from misskaty.helper.functions import (
 from misskaty.helper.localization import use_chat_lang
 from misskaty.vars import COMMAND_HANDLER, SUDO
 
-LOGGER = getLogger("MissKaty")
+LOGGER = getLogger("PeaMasamba")
 
 __MODULE__ = "Admin"
 __HELP__ = """
