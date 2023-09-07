9d7eedc28cf9a18735e2aaff26061efaf4c3965e
Pea Masamba <129601224+Yyg-Masamba@users.noreply.github.com>
2023-09-07 14:39:38 +0700

    fix

--- misskaty/plugins/misc_tools.py
+++ misskaty/plugins/misc_tools.py
@@ -1,8 +1,8 @@
 """
- * @author        yasir <yasiramunandar@gmail.com>
+ * @author        peamasamba<peamasamba@gmail.com>
  * @date          2022-12-01 09:12:27
- * @projectName   MissKatyPyro
- * Copyright @YasirPedia All rights reserved
+ * @projectName   Pea Masamba
+ * Copyright @peamasamba All rights reserved
 """
 
 import asyncio
@@ -41,7 +41,7 @@ from misskaty.helper.tools import rentry
 from misskaty.vars import COMMAND_HANDLER
 from utils import extract_user, get_file_id
 
-LOGGER = getLogger("MissKaty")
+LOGGER = getLogger("PeaMasamba")
 
 __MODULE__ = "Misc"
 __HELP__ = """
