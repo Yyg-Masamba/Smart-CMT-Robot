d6cda60dfdd52284f1395dc508c6e0844f154297
Pea Masamba <129601224+Yyg-Masamba@users.noreply.github.com>
2023-09-07 14:02:52 +0700

    fix

--- misskaty/plugins/bypass.py
+++ misskaty/plugins/bypass.py
@@ -1,9 +1,9 @@
 """
- * @author        yasir <yasiramunandar@gmail.com>
+ * @author         peamasamba<peamasamba@gmail.com>
  * @date          2022-12-01 09:12:27
  * @lastModified  2022-12-01 09:32:31
- * @projectName   MissKatyPyro
- * Copyright @YasirPedia All rights reserved
+ * @projectName   Pea Masamba
+ * Copyright @peamasamba All rights reserved
 """
 import re
 import urllib.parse
