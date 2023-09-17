f47ab909194a0ea46e9e1b23d036ed1f1aed288f
Pea Masamba <129601224+Yyg-Masamba@users.noreply.github.com>
2023-09-17 16:19:23 +0700

    Update auto_forwarder.py

--- misskaty/plugins/auto_forwarder.py
+++ misskaty/plugins/auto_forwarder.py
@@ -16,7 +16,7 @@ from misskaty.vars import (
     MINIMUM_FILE_SIZE,
 )
 
-LOGGER = getLogger("SmartMasambaBot")
+LOGGER = getLogger("Smart-CMT-Robot")
 
 
 async def FilterMessage(message: Message):
