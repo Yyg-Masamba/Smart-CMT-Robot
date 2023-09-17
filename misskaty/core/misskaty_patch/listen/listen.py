9c5a01c78aef7635f2499f7870be20fa678de0d0
Pea Masamba <129601224+Yyg-Masamba@users.noreply.github.com>
2023-09-17 16:32:10 +0700

    fix

--- misskaty/core/misskaty_patch/listen/listen.py
+++ misskaty/core/misskaty_patch/listen/listen.py
@@ -29,7 +29,7 @@ from pyrogram.errors import MessageIdInvalid
 
 from ..utils import PyromodConfig, patch, patchable
 
-logger = logging.getLogger("MissKaty")
+logger = logging.getLogger("Smart-CMT-Robot")
 
 
 class ListenerStopped(Exception):
