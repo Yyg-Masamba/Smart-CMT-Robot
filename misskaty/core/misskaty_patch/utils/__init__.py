2ab99676cfe93356eaa65fb8c5cb6d22da84f79f
Pea Masamba <129601224+Yyg-Masamba@users.noreply.github.com>
2023-12-06 11:18:37 +0700

    Delete misskaty directory

--- misskaty/core/misskaty_patch/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-# skipcq
-from .admin_utils import check_rights, is_admin
-from .get_user import get_user
-from .handler_error import handle_error
-from .utils import PyromodConfig, patch, patchable
