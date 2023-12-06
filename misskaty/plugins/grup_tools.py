106600b7a6886e06bad76a040ad94b9fe39a7ffe
Pea Masamba <129601224+Yyg-Masamba@users.noreply.github.com>
2023-12-06 11:37:20 +0700

    Update grup_tools.py

--- misskaty/plugins/grup_tools.py
+++ misskaty/plugins/grup_tools.py
@@ -22,7 +22,7 @@ from misskaty.helper import fetch, use_chat_lang
 from misskaty.vars import COMMAND_HANDLER, SUDO, SUPPORT_CHAT
 from utils import temp
 
-LOGGER = getLogger("MissKaty")
+LOGGER = getLogger("Smart-CMT-Robot")
 
 
 def circle(pfp, size=(215, 215)):
