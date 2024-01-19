77af475c5b04693587361da36e5ba4865f7e27fe
Pea Masamba <129601224+Yyg-Masamba@users.noreply.github.com>
2024-01-19 19:22:12 +0700

    'Refactored by Sourcery'

--- misskaty/plugins/web_scraper.py
+++ misskaty/plugins/web_scraper.py
@@ -1286,8 +1286,7 @@ async def kusonime_scrap(client, callback_query, strings):
         InlineButton(strings("cl_btn"), f"close#{callback_query.from_user.id}"),
     )
     try:
-        init_url = data_kuso.get(link, False)
-        if init_url:
+        if init_url := data_kuso.get(link, False):
             await callback_query.message.edit_msg(init_url.get("ph_url"), reply_markup=keyboard)
         tgh = await kuso.telegraph(link, client.me.username)
         data_kuso[link] = {"ph_url": tgh}
@@ -1369,17 +1368,15 @@ async def nodrakorddl_scrap(_, callback_query, strings):
             soup = BeautifulSoup(html.text, "lxml")
             if "/tv/" in link:
                 result = soup.find("div", {"entry-content entry-content-single"}).find_all("p")
-                msg = ""
-                for i in result:
-                    msg += str(f"{i}\n")
-                link = await post_to_telegraph(False, "Smart-CMT-Robot NoDrakor", msg)
+                msg = "".join(str(f"{i}\n") for i in result)
+                link = await post_to_telegraph(False, "MissKaty NoDrakor", msg)
                 return await callback_query.message.edit_msg(
                     strings("res_scrape").format(link=link, kl=link), reply_markup=keyboard
                 )
             res = soup.find_all(class_="button button-shadow")
             res = "".join(f"{i.text}\n{i['href']}\n\n" for i in res)
             if len(res) > 3500:
-                link = await post_to_telegraph(False, "Smart-CMT-Robot NoDrakor", res)
+                link = await post_to_telegraph(False, "MissKaty NoDrakor", res)
                 return await callback_query.message.edit_msg(
                     strings("res_scrape").format(link=link, kl=link), reply_markup=keyboard
                 )
