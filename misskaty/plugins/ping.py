57e58c8736f1b74dc1889a501d2ac841abc7bf31
Pea Masamba <129601224+Yyg-Masamba@users.noreply.github.com>
2024-03-01 17:16:59 +0700

    Fix ping DC ?

--- misskaty/plugins/ping.py
+++ misskaty/plugins/ping.py
@@ -25,11 +25,11 @@ PING_LOCK = Lock()
 async def ping(_, ctx: Message):
     currentTime = get_readable_time(time.time() - botStartTime)
     start_t = time.time()
-    rm = await ctx.reply_msg("🐱 Pong!!...")
+    rm = await ctx.reply_msg("🅿️ Pong!!...")
     end_t = time.time()
     time_taken_s = round(end_t - start_t, 3)
     await rm.edit_msg(
-        f"<b>🐈 MissKatyBot {misskaty_version} based Pyrogram {pyrover} Online.</b>\n\n<b>Ping:</b> <code>{time_taken_s} detik</code>\n<b>Uptime:</b> <code>{currentTime}</code>\n<b>Python Version:</b> <code>{platform.python_version()}</code>"
+        f"<b> Smart-CMT-Robot {misskaty_version} based Pyrogram {pyrover} Online.</b>\n\n<b>Ping:</b> <code>{time_taken_s} detik</code>\n<b>Uptime:</b> <code>{currentTime}</code>\n<b>Python Version:</b> <code>{platform.python_version()}</code>"
     )
 
 
