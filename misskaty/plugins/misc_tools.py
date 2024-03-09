047ae46457059d8f8e6b12efe0bc51f6e705ff24
Pea Masamba <129601224+Yyg-Masamba@users.noreply.github.com>
2024-03-09 17:39:46 +0700

    fix

--- misskaty/plugins/misc_tools.py
+++ misskaty/plugins/misc_tools.py
@@ -1,8 +1,8 @@
 """
- * @author        yasir <yasiramunandar@gmail.com>
- * @date          2022-12-01 09:12:27
- * @projectName   MissKatyPyro
- * Copyright @YasirPedia All rights reserved
+ * @author        Pea Masamba <comelmuewa831@gmail.com>
+ * @date          2024-01-17 22:12:27
+ * @projectName   Smart-CMT-Robot
+ * Copyright ©peamasamba All rights reserved
 """
 
 import asyncio
